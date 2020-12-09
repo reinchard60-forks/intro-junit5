57855fd2565d204c59bd9c181e32b4a68fea00fc
reinchard_lang <reinchard60@hotmail.com>
2020-12-08 19:58:18 -0600

    Instaccne Lifecycle Demo

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,14 +1,19 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
     private Greeting greeting;
 
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before - I am only called Once!!!");
+    }
+
     @BeforeEach
     void setUp() {
+        System.out.println("In Before Each...");
         greeting = new Greeting();
     }
 
@@ -21,4 +26,20 @@ class GreetingTest {
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("John"));
     }
+
+    @Test
+    void testHelloWorld1() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each....");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("After - I am only called Once!!!");
+    }
+
 }
